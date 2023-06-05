87a0ee8e9fd95707756b142a9d7f8b4045a97b26
Michael Scott Davidson <lvrjautodata@gmail.com>
2023-06-05 16:54:23 -0700

    Create lvmpd_nibrs_GITHUB.R

---
+++ lvmpd_nibrs_GITHUB.R
@@ -0,0 +1,125 @@
+#Clear workspace
+rm(list=ls())
+
+library(tidyverse)
+library(rjson)
+library(httr)
+library(janitor)
+
+####
+####
+####
+
+#Create master dataframe
+lvmpd_nibrs_main <- NULL
+
+#Build sequence of days. This moves backwards from current date.
+##So the zero value is current date. Goes back about a month.  
+x <- seq(0,31)
+
+#For Loop
+
+for (i in x) {
+  
+  #Create URL
+  ##It wants a range of two days, but I only want 1 day, so I feed in the "day" value twice
+  
+  url <- paste0("https://services.arcgis.com/jjSk6t82vIntwDbs/arcgis/rest/services/Weekly_Public_Crimes/FeatureServer/0/query?where=%20(DateDif%20%3D%20",
+                i,
+                "%20OR%20DateDif%20%3D%20",
+                i,
+                ")%20&outFields=*&outSR=4326&f=json")
+  
+  Sys.sleep(1)
+
+  #Load in the data via API Call
+  url_get <- httr::GET(url)
+  
+  lvmpd_content <- httr::content(url_get, 
+                                 as = "text")
+  
+  #Start parsing JSON file
+  ##Be sure to flatten it
+  lvmpd_JSON <- jsonlite::fromJSON(lvmpd_content, 
+                                   flatten = TRUE)
+  
+  #Turn into dataframe
+  lvmpd_DF <- (lvmpd_JSON$features) %>% 
+    as.data.frame() %>% 
+    #Start cleaning column names
+    clean_names()
+  
+  #Finish cleaning column names
+  colnames(lvmpd_DF) <- gsub("attributes_", "",
+                             colnames(lvmpd_DF))
+  
+  #Bind the new scrape's data
+  lvmpd_nibrs_main <- rbind(lvmpd_nibrs_main, 
+                        lvmpd_DF)
+  
+  #Wait 1 second before running again
+  Sys.sleep(1)
+}
+
+####
+####
+####
+
+#Fix the date/time columns
+lvmpd_nibrs_main <- lvmpd_nibrs_main %>% 
+  #Get only first 10 characters
+  mutate(reported_on = substr(reported_on, 1, 10)) %>% 
+  #Convert into correct date
+  mutate(reported_on = as.POSIXct(as.numeric(reported_on), 
+                                    origin="1970-01-01")) 
+
+#Drop the objectid columns
+lvmpd_nibrs_main <- lvmpd_nibrs_main %>% 
+  select(-updated_date, date_dif)
+
+#Clean up certain column values
+lvmpd_nibrs_main <- lvmpd_nibrs_main %>% 
+  mutate(location = str_squish(toupper(location))) %>% 
+  mutate(csz = str_squish(toupper(csz)))
+
+####
+####
+####
+#UPDATE RESULTS ON GITHUB
+
+#Write the CSV name (same as before)
+lvmpd_nibrs_all_path <- "data/lvmpd_nibrs_all.csv"
+
+#Load in Rolling File from repository
+lvmpd_nibrs_all <- read_csv("data/lvmpd_nibrs_all.csv") %>%
+  mutate_all(as.character)
+
+#Make all the data "characters"                      
+lvmpd_nibrs_main <- lvmpd_nibrs_main %>%
+  mutate_all(as.character)
+
+#Rbind the newest results to it
+lvmpd_nibrs_all_new <- rbind(lvmpd_nibrs_all,
+                             lvmpd_nibrs_main) %>% 
+  #Delete any repeat objects
+  distinct(object_id, .keep_all = TRUE)
+
+#Write new CSV for repository
+write.csv(lvmpd_nibrs_all_new, 
+          lvmpd_nibrs_all_path, 
+          row.names=FALSE)
+
+####
+####
+####
+#UPLOAD ROLLING 2023 RESULTS TO GOOGLE DRIVE
+DRIVE_JSON <- Sys.getenv("DRIVE_JSON")
+DRIVE_FOLDER <- Sys.getenv("DRIVE_FOLDER")
+
+googledrive::drive_auth(path = DRIVE_JSON)
+td <- drive_get(DRIVE_FOLDER)
+
+drive_put(lvmpd_nibrs_all_path, 
+          name =  "lvmpd_nibrs_all", 
+          type = "spreadsheet", 
+          path=as_id(td))
