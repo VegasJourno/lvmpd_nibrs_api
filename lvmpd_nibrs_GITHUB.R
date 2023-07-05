1e45226b50f44998c40cc8462d067c98602aafde
Michael Scott Davidson <michaeldmedia@gmail.com>
2023-07-05 12:40:05 -0700

    Update lvmpd_nibrs_GITHUB.R

--- lvmpd_nibrs_GITHUB.R
+++ lvmpd_nibrs_GITHUB.R
@@ -129,30 +129,3 @@ drive_put(lvmpd_nibrs_all_path,
           name =  "lvmpd_nibrs_all", 
           type = "spreadsheet", 
           path=as_id(td))
-
-####
-####
-####
-
-#Read in the environment Secret objects
-GMAIL_SENDER <- Sys.getenv("GMAIL_SENDER")
-GMAIL_RECIPIENT <- Sys.getenv("GMAIL_RECIPIENT")
-GMAIL_USER <- Sys.getenv("GMAIL_USER")
-GMAIL_PASS <- Sys.getenv("GMAIL_PASS")
-
-#Get the DateTime that file was exported
-ExportDateTime <- format((Sys.time() - (6*60*60)), 
-                         "%Y-%m-%d_%I%p")
-
-#Email notification of success
-send.mail(from = GMAIL_SENDER,
-          to = GMAIL_SENDER,
-          subject = paste0("Github Success: LVMPD NIBRS Export - ", ExportDateTime),
-          body = "Github Action ran successfully.",
-          smtp = list(host.name = "smtp.gmail.com", port = 465, 
-                      user.name = GMAIL_USER, 
-                      #Generated app password thru Gmail security settings
-                      passwd = GMAIL_PASS, 
-                      ssl = TRUE),
-          authenticate = TRUE,
-          send = TRUE)
